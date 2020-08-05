1e221f949ec7d079e7414f35af680fa42924e87c
MonikaGu <63239253+MonikaGu@users.noreply.github.com>
2020-08-05 10:22:06 +0300

    last commit

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php 
     session_start(); 
 
-    // logout logic
+    // logout 
     if(isset($_GET['action']) and $_GET['action'] == 'logout'){
         session_start();
         unset($_SESSION['username']);
